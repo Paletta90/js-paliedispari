14f6e887071e793b5e4dad2413f0b201a366fb9a
DavideDiPietro <davide.dipietro3@gmail.com>
2022-03-18 15:38:14 +0100

    Palindromo

---
+++ Palindroma/main.js
@@ -0,0 +1,29 @@
+let container = document.getElementById("container");
+
+let word = prompt("Inserire una parola:");
+
+checkPalindroma(word);
+
+function checkPalindroma(word) {
+
+    // La se quenza consiste in: 
+    // - String(word): Word è una stringa 
+    // - .replace() Levo gli spazi
+    // - .split(""): Word diventa un array dove ogni posizione 
+    // - .reverse(): Rigiro l'array 
+    // - .join(): Trasformo l'array in una stringa
+
+    const reverseWord = String(word).replace(/\s+/g, '').split('').reverse().join('');
+
+    wordNoSpace = String(word).replace(/\s+/g, '');
+
+    console.log(reverseWord);
+    // console.log(word);
+
+    // Confronto se la parola inserita e quella capovolta sono uguali
+    if (wordNoSpace.toUpperCase() == reverseWord.toUpperCase()) {
+        container.innerHTML = `${word} è palindromo`;
+    } else {
+        container.innerHTML = `${word} non è palindromo`;
+    }
+}
